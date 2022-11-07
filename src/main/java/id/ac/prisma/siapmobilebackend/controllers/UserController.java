6f92c58a52f857c6a73ab73208a1d3dff9799348
nofrets <opetstudio@gmail.com>
2022-11-07 10:02:16 +0800

    develop api user

--- src/main/java/id/ac/prisma/siapmobilebackend/controllers/UserController.java
+++ src/main/java/id/ac/prisma/siapmobilebackend/controllers/UserController.java
@@ -3,13 +3,11 @@ package id.ac.prisma.siapmobilebackend.controllers;
 import id.ac.prisma.siapmobilebackend.data.model.TbUser;
 import id.ac.prisma.siapmobilebackend.data.repo.TbUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = {"/user"})
@@ -17,6 +15,48 @@ public class UserController {
 
     @Autowired
     TbUserRepository tbUserRepository;
+
+    @RequestMapping(value = "/by-id/{id}", method = RequestMethod.GET, produces = "application/json")
+    public Map getUserById(@PathVariable("id") Integer id) {
+        Map response = new HashMap();
+        TbUser tbUser = tbUserRepository.findById(id).orElse(null);
+        response.put("data", tbUser);
+        return response;
+    }
+
+    @RequestMapping(value = "/by-email", method = RequestMethod.POST, produces = "application/json")
+    public Map byEmail(@RequestBody Map bodyRequest) {
+        // query data user berdasarkan email
+        // call tbUserRepository.findByEmail
+        String email = bodyRequest.get("email").toString();
+        Optional<TbUser> tbUser = tbUserRepository.findByEmail(email);
+        TbUser result = tbUser.orElse(null);
+
+        Map response = new HashMap();
+        response.put("data", result);
+        return response;
+    }
+
+    @RequestMapping(value = "/create-user", method = RequestMethod.POST, produces = "application/json")
+    public Map createUser(@RequestBody Map bodyRequest) {
+        // insert data
+        // call tbUserRepository.save
+        TbUser tbUser = new TbUser();
+        tbUser.setFullName(bodyRequest.get("full_name").toString());
+        tbUser.setEmail(bodyRequest.get("email").toString());
+        tbUser.setPassword(bodyRequest.get("password").toString());
+        tbUser = tbUserRepository.save(tbUser);
+
+        Map response = new HashMap();
+        response.put("data", tbUser);
+        return response;
+    }
+
+
+
+
+
+
 
     @RequestMapping(value = "/siswa", method = RequestMethod.GET, produces = "application/json")
     public Map siswa() {
@@ -28,20 +68,10 @@ public class UserController {
     public Map payment(@RequestBody Map data) {
         String myName = (String) data.get("yourname");
         Map response = new HashMap();
-
-
-
-
-
-
-
-
-
         response.put("status", "success");
         response.put("customer", myName);
         return response;
     }
-
     @RequestMapping(value = "/get-all-users", method = RequestMethod.GET, produces = "application/json")
     public Iterable<TbUser> getAllUsers(){
         Iterable<TbUser> allUser = tbUserRepository.findAll();
--- src/main/java/id/ac/prisma/siapmobilebackend/data/repo/TbUserRepository.java
+++ src/main/java/id/ac/prisma/siapmobilebackend/data/repo/TbUserRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface TbUserRepository extends CrudRepository<TbUser, Integer> {
     Optional<TbUser> findByEmail(String email);
+    Optional<TbUser> findByFullName(String fullName);
 }
